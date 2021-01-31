9d37afd5e7d1806216a2c28940f02be5ef8c6017
Prarabdh Garg <prarabdhgrg@gmail.com>
2021-01-31 11:22:40 +0530

    Complete assignment 1

--- Membership Prototcol 1 (Basic Concepts 1 Assignment)/MP1Node.cpp
+++ Membership Prototcol 1 (Basic Concepts 1 Assignment)/MP1Node.cpp
@@ -215,9 +215,111 @@ void MP1Node::checkMessages() {
  * DESCRIPTION: Message handler for different message types
  */
 bool MP1Node::recvCallBack(void *env, char *data, int size ) {
-	/*
-	 * Your code goes here
-	 */
+    MessageHdr *header = (MessageHdr *) data;
+    if(header ->  msgType == JOINREQ) {
+        // Extract fields from data message
+        int *id = (int *) (data + sizeof(MessageHdr));
+        short *port = (short *) (data + sizeof(MessageHdr) + sizeof(int));
+        long *heartbeat = (long *) (data + sizeof(MessageHdr) + sizeof(int) + sizeof(short));
+
+        // Add the node to local membership list if it doesn't already exist. Update if already present
+        Address address;
+        memset(&address, 0, sizeof(Address));
+        *((int *)(&address.addr)) = *id;
+        *(short *)(&address.addr[4]) = *port;
+        bool found = false;
+        for(int i = 0; i < memberNode -> memberList.size() && !found; i++) {
+            if(memberNode -> memberList[i].getid() == *id && memberNode -> memberList[i].getport() == *port) {
+                if(memberNode -> memberList[i].heartbeat < *heartbeat) {
+                    memberNode -> memberList[i].setheartbeat(*heartbeat);
+                    memberNode -> memberList[i].settimestamp(par->getcurrtime());
+                }
+                found = true;
+            }
+        }
+        if(!found) {
+            MemberListEntry memberListEntry = MemberListEntry(*id, *port, *heartbeat, par->getcurrtime());
+            memberNode -> memberList.push_back(memberListEntry);
+            log -> logNodeAdd(&(memberNode -> addr), &address);
+        }
+
+        // Generate a JOINREP message
+        size_t msgSize = (sizeof(MessageHdr) + sizeof(memberNode->addr.addr) + (memberNode->memberList.size() * sizeof(MemberListEntry))) * sizeof(char);
+        void *msg = (MessageHdr *) malloc(msgSize);
+        void * p = msg;
+        ((MessageHdr *)msg)->msgType = JOINREP;
+        msg = msg + sizeof(MessageHdr);
+        memcpy((char *)(msg), &memberNode->addr.addr, sizeof(memberNode->addr.addr));
+        msg = msg + sizeof(memberNode->addr.addr);
+        for(int i = 0; i < memberNode->memberList.size(); i++) {
+            printf("Coppying member %d\n", i);
+            memcpy(msg, &(memberNode->memberList[i]), sizeof(MemberListEntry));
+            msg = msg + sizeof(MemberListEntry);
+        }
+        emulNet->ENsend(&(memberNode->addr), &address, (char *) p, msgSize);
+        // free(msg);
+    } else if (header -> msgType == JOINREP) {
+        // Membership request of the node has been accepted
+        memberNode -> inGroup = true;
+        printf("In message reply\n");
+        // Extract peer information contained in the message
+        data = data + sizeof(MessageHdr) + sizeof(memberNode->addr.addr);
+        if((size - sizeof(MessageHdr) - sizeof(memberNode->addr.addr)) > 0) {
+            int i = (size - sizeof(MessageHdr) - sizeof(memberNode->addr.addr)) / sizeof(MemberListEntry);
+            while(i--) {
+                printf("scanning data");
+                MemberListEntry *temp = (MemberListEntry *) malloc(sizeof(MemberListEntry));
+                memcpy(temp, data, sizeof(MemberListEntry));
+                Address address;
+                memset(&address, 0, sizeof(Address));
+                *((int *)(&address.addr)) = temp->id;
+                *(short *)(&address.addr[4]) = temp->port;
+                bool found = false;
+                for(int i = 0; i < memberNode -> memberList.size() && !found; i++) {
+                    if(memberNode -> memberList[i].getid() == temp->id && memberNode -> memberList[i].getport() == temp->port) {
+                        if(memberNode -> memberList[i].heartbeat < temp->heartbeat) {
+                            memberNode -> memberList[i].setheartbeat(temp->heartbeat);
+                            memberNode -> memberList[i].settimestamp(par->getcurrtime());
+                        }
+                        found = true;
+                    }
+                }
+                if(!found) {
+                    MemberListEntry memberListEntry = MemberListEntry(temp->id, temp->port, temp->heartbeat, par->getcurrtime());
+                    memberNode -> memberList.push_back(memberListEntry);
+                    log -> logNodeAdd(&(memberNode -> addr), &address);
+                }
+                data = data + sizeof(MemberListEntry);
+                free(temp);
+            }
+        }
+    } else if(header -> msgType == PING) {
+        // Extract fields from data message
+        int *id = (int *) (data + sizeof(MessageHdr));
+        short *port = (short *) (data + sizeof(MessageHdr) + sizeof(int));
+        long *heartbeat = (long *) (data + sizeof(MessageHdr) + sizeof(int) + sizeof(short));
+
+        // Add the node to local membership list if it doesn't already exist. Update if already present
+        Address address;
+        memset(&address, 0, sizeof(Address));
+        *((int *)(&address.addr)) = *id;
+        *(short *)(&address.addr[4]) = *port;
+        bool found = false;
+        for(int i = 0; i < memberNode -> memberList.size() && !found; i++) {
+            if(memberNode -> memberList[i].getid() == *id && memberNode -> memberList[i].getport() == *port) {
+                if(memberNode -> memberList[i].heartbeat < *heartbeat) {
+                    memberNode -> memberList[i].setheartbeat(*heartbeat);
+                    memberNode -> memberList[i].settimestamp(par->getcurrtime());
+                }
+                found = true;
+            }
+        }
+        if(!found) {
+            MemberListEntry memberListEntry = MemberListEntry(*id, *port, *heartbeat, par->getcurrtime());
+            memberNode -> memberList.push_back(memberListEntry);
+            log -> logNodeAdd(&(memberNode -> addr), &address);
+        }
+    }
 }
 
 /**
@@ -229,9 +331,36 @@ bool MP1Node::recvCallBack(void *env, char *data, int size ) {
  */
 void MP1Node::nodeLoopOps() {
 
-	/*
-	 * Your code goes here
-	 */
+    // Increment node's heartbeat value
+    memberNode->heartbeat = memberNode->heartbeat + 1;
+    
+    // Check if ping from some node has not been received
+    vector<MemberListEntry>::iterator i = memberNode->memberList.begin();
+    for(; i != memberNode -> memberList.end(); i++) {
+        Address address;
+        memset(&address, 0, sizeof(Address));
+        *((int *)(&address.addr)) = i -> id;
+        *(short *)(&address.addr[4]) = i -> port;
+        if(par->getcurrtime() - i -> timestamp >= TREMOVE) {
+            memberNode->memberList.erase(i);
+            log->logNodeRemove(&(memberNode -> addr), &address);
+        }
+    }
+
+    // Send a PING message to all other nodes in the group
+    for(int i = 0; i < memberNode -> memberList.size(); i++) {
+        Address address;
+        memset(&address, 0, sizeof(Address));
+        *((int *)(&address.addr)) = memberNode -> memberList[i]. id;
+        *(short *)(&address.addr[4]) = memberNode -> memberList[i].port;
+        size_t msgsize = sizeof(MessageHdr) + sizeof(memberNode->addr.addr) + sizeof(long) + 1;
+        MessageHdr *msg = (MessageHdr *) malloc(msgsize * sizeof(char));
+        msg->msgType = PING;
+        memcpy((char *)(msg+1), &memberNode->addr.addr, sizeof(memberNode->addr.addr));
+        memcpy((char *)(msg+1) + 1 + sizeof(memberNode->addr.addr), &memberNode->heartbeat, sizeof(long));
+        emulNet->ENsend(&(memberNode->addr), &address, (char *) msg, msgsize);
+        free(msg);
+    }
 
     return;
 }
